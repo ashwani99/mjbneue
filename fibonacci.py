e23492556c99944771f62111bdc5f306e0341c22
Jason Braganza <jason@janusworx.com>
2017-08-01 12:16:52 +0530

    General save

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,3 @@
-
 print ("""1. Do you want to hear a story about the Golden Mean?
 or
 2. A gist of one of the main principles of the 11th century treatise
@@ -11,19 +10,23 @@ user_choice = input("Pick 1 or 2 > ")
 
 while True:
     if user_choice == '1':
-        print(user_choice)
+        print("\nSucker! It’s the same story. :) ")
+        input("\nPress the Enter/Return key to go back in time!")
+        print()
         break
     elif user_choice == '2':
-        print(user_choice)
         break
-    user_choice = input('Pick 1 or 2? >')
+    else:
+        user_choice = input('Pick 1 or 2? >')
 
-# if user_choice == 1:
-#     print "Hello!"
-# elif user_choice == 2:
-#     print "Hola!"
 
-print ("""To see examples of the Golden Mean in real life,
-open the link below in your browser,
+print ("\n""""Liber Abaci (1202, also spelled as Liber Abbaci) is a historic
+book on arithmetic by Leonardo of Pisa""")
 
-https://www.google.com/search?site=&tbm=isch&source=hp&biw=1440&bih=788&q=fibonacci+in+real+life&oq=fibonacci+in+&gs_l=img.3.1.0l10.1957.5728.0.7080.9.9.0.0.0.0.106.900.3j6.9.0....0...1.1.64.img..0.9.899.4ESLA1Lr4aA""")
+
+
+# input("Press the Enter/Return key to continue")
+# print ("\n""""To see examples of the Golden Mean in real life,
+# open the link below in your browser,
+#
+# https://www.google.com/search?site=&tbm=isch&source=hp&biw=1440&bih=788&q=fibonacci+in+real+life&oq=fibonacci+in+&gs_l=img.3.1.0l10.1957.5728.0.7080.9.9.0.0.0.0.106.900.3j6.9.0....0...1.1.64.img..0.9.899.4ESLA1Lr4aA""")
