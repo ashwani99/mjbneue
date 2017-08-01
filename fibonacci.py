a3c64f3686e19e4947b4ca2c8a177f8787f8579b
Jason Braganza <jason@janusworx.com>
2017-08-01 10:37:55 +0530

    Starting with a while loop to set up conditions
    Signed-off-by: Jason Braganza <jason@janusworx.com>

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+
+print ("""1. Do you want to hear a story about the Golden Mean?
+or
+2. A gist of one of the main principles of the 11th century treatise
+“Liber Abaci”?
+""")
+
+# user_choice = int(input("Pick 1 or 2 > "))
+
+user_choice = input("Pick 1 or 2 > ")
+
+
+# while user_choice == 1 or user_choice == 2
+#     break
+
+# if user_choice == 1:
+#     print "Hello!"
+# elif user_choice == 2:
+#     print "Hola!"
+
+print ("""To see examples of the Golden Mean in real life,
+open the link below in your browser,
+
+https://www.google.com/search?site=&tbm=isch&source=hp&biw=1440&bih=788&q=fibonacci+in+real+life&oq=fibonacci+in+&gs_l=img.3.1.0l10.1957.5728.0.7080.9.9.0.0.0.0.106.900.3j6.9.0....0...1.1.64.img..0.9.899.4ESLA1Lr4aA""")
