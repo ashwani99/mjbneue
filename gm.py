8b081c55eea970c17547ed1e7d959823bfac30f2
Jason Braganza <jason@janusworx.com>
2017-08-01 15:21:51 +0530

    General save

--- gm.py
+++ gm.py
@@ -1,40 +1,65 @@
-print ("""This is a slightly long winded story and I still haven’t learnt
-how to page screens so when you see a prompt like this ">"
-just press the Enter/Return key
+import time #Doing this from some SO answer, beacuse I want to pause my script.
+
+
+# print ("""This is a slightly long winded story and I still haven’t learnt
+# how to page screens so when you see a prompt like this ">"
+# just press the Enter/Return key
+# """)
+# input("> ")
+# print ("""1. A gist from one of the chapters of the 11th century treatise
+# “Liber Abaci”
+# or?
+# 2. Do you want to hear a story about the Golden Mean?""")
+#
+# # user_choice = int(input("Pick 1 or 2 > "))
+#
+# user_choice = input("Pick 1 or 2 > ")
+#
+# while True:
+#     if user_choice == '1':
+#         print("\nSucker! It’s the same story. :) ")
+#         input("\nPress the Enter/Return key to go back in time! > ")
+#         break
+#     elif user_choice == '2':
+#         break
+#     else:
+#         user_choice = input('Pick 1 or 2? >')
+
+
+# print ("\n""""Liber Abaci (1202, also spelled as Liber Abbaci) is a historic
+# book on arithmetic by Leonardo of Pisa""")
+# time.sleep(2)
+# print ("also known as Fibonacci!")
+# time.sleep(2)
+# print ("""Another example in this chapter, describing the growth of a population of
+# rabbits, was the origin of the Fibonacci sequence for which the author is most
+# famous today.""")
+# input("Press the Enter/Return key to continue")
+# print("\n""""You know Fibonacci numbers""")
+# print("""while there are lots of other numbers that can be summed in a series,
+# only the Fibonacci series, (i.e the sequence that starts with 0 & 1) approaches
+# what we call the Golden Ratio (symbolised \u03C6 or phi)""")
+#
+# print("""So just what is the Golden Ratio""")
+# print("""The golden ratio is a special number approximately equal to
+# 1.6180339887498948482. """)
+print("""or more accurately,  1 + sqrt\{5}
+                     ------------
+                           2""")
+print("""To illustrate:
+-----------------------------------
+A            B                    C
+
+The ratio of the long segment (BC) to the short segment (AC) is the *same* as
+the ratio of the total segment (AC) to the long segment (BC)
 """)
-input("> ")
-print ("""1. Do you want to hear a story about the Golden Mean?
-or
-2. A gist of one of the main principles of the 11th century treatise
-“Liber Abaci”?
-""")
-
-# user_choice = int(input("Pick 1 or 2 > "))
-
-user_choice = input("Pick 1 or 2 > ")
-
-while True:
-    if user_choice == '1':
-        print("\nSucker! It’s the same story. :) ")
-        input("\nPress the Enter/Return key to go back in time! > ")
-        print()
-        break
-    elif user_choice == '2':
-        break
-    else:
-        user_choice = input('Pick 1 or 2? >')
+print("""To illustrate:""")
+print("""To illustrate:""")
 
 
-print ("\n""""Liber Abaci (1202, also spelled as Liber Abbaci) is a historic
-book on arithmetic by Leonardo of Pisa""")
-input("> ")
-print ("also known as Fibonacci!")
-
-
-
+# print ("""To hear it told even more beautifully, watch this video
+# https://www.youtube.com/watch?v=O2wU-HT7FiM""")
 
 # input("Press the Enter/Return key to continue")
 # print ("\n""""To see examples of the Golden Mean in real life,
 # open the link below in your browser,
-#
-# https://www.google.com/search?site=&tbm=isch&source=hp&biw=1440&bih=788&q=fibonacci+in+real+life&oq=fibonacci+in+&gs_l=img.3.1.0l10.1957.5728.0.7080.9.9.0.0.0.0.106.900.3j6.9.0....0...1.1.64.img..0.9.899.4ESLA1Lr4aA""")
